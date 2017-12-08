b7654938043a2ca13f66cfffdda3993be30ed6f1
Tuan Do <tuandn@brandeis.edu>
2017-12-08 14:27:29 -0500

    Add lexicon translations

--- projects/vipa/Lexicon.hs
+++ projects/vipa/Lexicon.hs
@@ -14,172 +14,172 @@ data Feat =  Past | Futu  | Prog | Pres | Moda | Nega | Nom | AccOrDat
 lexicon :: String -> [Cat]
 
 
-lexicon "chúng"   	= [Cat "chung"   	"NP" [] []]
-lexicon "họ"  		= [Cat "ho"  		"NP" [] []]
+lexicon "chúng"   	= [Cat "chung"   	"NP" [] []]         -- they/them (negative connotation)
+lexicon "họ"  		= [Cat "ho"  		"NP" [] []]         -- they/them
 
-lexicon "bống"    	= [Cat "bong"  		"CN" [] []]
-lexicon "bụt"    	= [Cat "but"  		"CN" [] []]
-lexicon "cám"    	= [Cat "cam"  		"CN" [] []]
-lexicon "tấm"    	= [Cat "tam"  		"CN" [] []]
-lexicon "hai"   	= [Cat "hai"   		"CD" [] []]
-lexicon "đôi"     	= [Cat "doi"     	"MW" [] []]
+lexicon "bống"    	= [Cat "bong"  		"CN" [] []]         -- goby/fish name
+lexicon "bụt"    	= [Cat "but"  		"CN" [] []]         -- Buddha
+lexicon "cám"    	= [Cat "cam"  		"CN" [] []]         -- person's name
+lexicon "tấm"    	= [Cat "tam"  		"CN" [] []]         -- person's name
+lexicon "hai"   	= [Cat "hai"   		"CD" [] []]         -- two
+lexicon "đôi"     	= [Cat "doi"     	"MW" [] []]         -- measure word, mean double
 
-lexicon "người"		= [Cat "nguoi" 		"CN" [] []]
-lexicon "bố"   		= [Cat "bo"   		"CN" [] []]
-lexicon "cha"   	= [Cat "cha"   		"CN" [] []]
-lexicon "mẹ"   		= [Cat "me"   		"CN" [] []]
-lexicon "mẹ-ghẻ"	= [Cat "me-ghe"		"CN" [] []]
-lexicon "con"   	= [Cat "con"   		"CN" [] []]
-lexicon "chị"  		= [Cat "chi"   		"CN" [] []]
-lexicon "em"   		= [Cat "em"   		"CN" [] []]
-lexicon "bạn"		= [Cat "ban"		"CN" [] []]
-lexicon "vua"  		= [Cat "vua"   		"CN" [] []]
-lexicon "hoàng-hậu" = [Cat "hoang-hau"	"CN" [] []]
-lexicon "lời"  		= [Cat "loi"   		"CN" [] []]
-lexicon "cá" 		= [Cat "ca"   		"CN" [] []]
-lexicon "chim" 		= [Cat "chim"  		"CN" [] []]
-lexicon "xác"  		= [Cat "xac"   		"CN" [] []]
-lexicon "xương"		= [Cat "xuong" 		"CN" [] []]
-lexicon "nước" 		= [Cat "nuoc"  		"CN" [] []]
-lexicon "mắm"  		= [Cat "mam"   		"CN" [] []]
-lexicon "nước-sôi"	= [Cat "nuoc-soi"	"CN" [] []]
-lexicon "gạo"   	= [Cat "gao"   		"CN" [] []]
-lexicon "thóc" 		= [Cat "thoc"  		"CN" [] []]
-lexicon "áo"   		= [Cat "ao"   		"CN" [] []]
-lexicon "quần" 		= [Cat "quan"  		"CN" [] []]
-lexicon "giày" 		= [Cat "giay"  		"CN" [] []]
-lexicon "lễ"   		= [Cat "le"   		"CN" [] []]
-lexicon "hội"   	= [Cat "hoi"   		"CN" [] []]
-lexicon "giỗ"  		= [Cat "gio"   		"CN" [] []]
-lexicon "cây"  		= [Cat "cay"   		"CN" [] []]
-lexicon "quả"  		= [Cat "qua"   		"CN" [] []]
-lexicon "cau"  		= [Cat "cau"   		"CN" [] []]
-lexicon "thị"  		= [Cat "thi"   		"CN" [] []]
-lexicon "chân" 		= [Cat "chan"  		"CN" [] []]
-lexicon "giường"	= [Cat "giuong"		"CN" [] []]
-lexicon "phòng"		= [Cat "phong" 		"CN" [] []]
-lexicon "nhà"  		= [Cat "nha"   		"CN" [] []]
-lexicon "cung" 		= [Cat "cung"  		"CN" [] []]
-lexicon "ao"   		= [Cat "ao"   		"CN" [] []]
+lexicon "người"		= [Cat "nguoi" 		"CN" [] []]         -- human
+lexicon "bố"   		= [Cat "bo"   		"CN" [] []]         -- father
+lexicon "cha"   	= [Cat "cha"   		"CN" [] []]         -- father
+lexicon "mẹ"   		= [Cat "me"   		"CN" [] []]         -- mother
+lexicon "mẹ-ghẻ"	= [Cat "me-ghe"		"CN" [] []]         -- stepmother
+lexicon "con"   	= [Cat "con"   		"CN" [] []]         -- son/daughter
+lexicon "chị"  		= [Cat "chi"   		"CN" [] []]         -- elder sister
+lexicon "em"   		= [Cat "em"   		"CN" [] []]         -- younger sister
+lexicon "bạn"		= [Cat "ban"		"CN" [] []]         -- friend
+lexicon "vua"  		= [Cat "vua"   		"CN" [] []]         -- king
+lexicon "hoàng-hậu" = [Cat "hoang-hau"	"CN" [] []]         -- queen
+lexicon "lời"  		= [Cat "loi"   		"CN" [] []]         -- story
+lexicon "cá" 		= [Cat "ca"   		"CN" [] []]         -- fish
+lexicon "chim" 		= [Cat "chim"  		"CN" [] []]         -- bird
+lexicon "xác"  		= [Cat "xac"   		"CN" [] []]         -- dead body
+lexicon "xương"		= [Cat "xuong" 		"CN" [] []]         -- bone
+lexicon "nước" 		= [Cat "nuoc"  		"CN" [] []]         -- water
+lexicon "mắm"  		= [Cat "mam"   		"CN" [] []]         -- fish sauce
+lexicon "nước-sôi"	= [Cat "nuoc-soi"	"CN" [] []]         -- boild water
+lexicon "gạo"   	= [Cat "gao"   		"CN" [] []]         -- rice
+lexicon "thóc" 		= [Cat "thoc"  		"CN" [] []]         -- whole rice
+lexicon "áo"   		= [Cat "ao"   		"CN" [] []]         -- shirt
+lexicon "quần" 		= [Cat "quan"  		"CN" [] []]         -- pants
+lexicon "giày" 		= [Cat "giay"  		"CN" [] []]         -- shoes
+lexicon "lễ"   		= [Cat "le"   		"CN" [] []]         -- festival
+lexicon "hội"   	= [Cat "hoi"   		"CN" [] []]         -- festival
+lexicon "giỗ"  		= [Cat "gio"   		"CN" [] []]         -- veneration of ancestors' death
+lexicon "cây"  		= [Cat "cay"   		"CN" [] []]         -- tree
+lexicon "quả"  		= [Cat "qua"   		"CN" [] []]         -- fruit
+lexicon "cau"  		= [Cat "cau"   		"CN" [] []]         -- a kind of fruit
+lexicon "thị"  		= [Cat "thi"   		"CN" [] []]         -- a kind of fruit
+lexicon "chân" 		= [Cat "chan"  		"CN" [] []]         -- legs
+lexicon "giường"	= [Cat "giuong"		"CN" [] []]         -- bed
+lexicon "phòng"		= [Cat "phong" 		"CN" [] []]         -- room
+lexicon "nhà"  		= [Cat "nha"   		"CN" [] []]         -- house
+lexicon "cung" 		= [Cat "cung"  		"CN" [] []]         -- harem
+lexicon "ao"   		= [Cat "ao"   		"CN" [] []]         -- pond
 
-lexicon "cùng-cha-khác-mẹ"	= [Cat "cung-cha-khac-me"		"ADJ" [] []]
-lexicon "đẹp"		= [Cat "dep" 		"ADJ" [] []]
-lexicon "ngon"  	= [Cat "ngon"  		"ADJ" [] []]
+lexicon "cùng-cha-khác-mẹ"	= [Cat "cung-cha-khac-me"		"ADJ" [] []]         -- paternal half-sibling
+lexicon "đẹp"		= [Cat "dep" 		"ADJ" [] []]         -- beautiful
+lexicon "ngon"  	= [Cat "ngon"  		"ADJ" [] []]         -- delicious (food)
 
-lexicon "đã"	    = [Cat "da"		    "AUX" [Past] []]
-lexicon "đang"	    = [Cat "dang"	    "AUX" [Prog] []]
-lexicon "sẽ"	    = [Cat "se"		    "AUX" [Futu] []]
-lexicon "không-thể" = [Cat "khong-the"  "AUX" [Moda] []]
-lexicon "không"	    = [Cat "khong"	    "AUX" [Nega] []]
-lexicon "chỉ"	    = [Cat "chi"	    "AUX" [Pres] []]
-lexicon "lại"	    = [Cat "lai"	    "AUX" [Pres] []]
-lexicon "rất"	    = [Cat "rat"	    "AUX" [Pres] []]
-lexicon "suốt-ngày" = [Cat "suot-ngay"  "AUX" [Pres] []]
+lexicon "đã"	    = [Cat "da"		    "AUX" [Past] []]         -- 
+lexicon "đang"	    = [Cat "dang"	    "AUX" [Prog] []]         -- 
+lexicon "sẽ"	    = [Cat "se"		    "AUX" [Futu] []]         -- 
+lexicon "không-thể" = [Cat "khong-the"  "AUX" [Moda] []]         -- can't 
+lexicon "không"	    = [Cat "khong"	    "AUX" [Nega] []]         -- don't/not
+lexicon "chỉ"	    = [Cat "chi"	    "AUX" [Pres] []]         -- only
+lexicon "lại"	    = [Cat "lai"	    "AUX" [Pres] []]         -- again
+lexicon "rất"	    = [Cat "rat"	    "AUX" [Pres] []]         -- very
+lexicon "suốt-ngày" = [Cat "suot-ngay"  "AUX" [Pres] []]         -- everyday
 
-lexicon "trong"		= [Cat "trong" 		"PREP" [] []]
-lexicon "cho"   	= [Cat "cho"   		"PREP" [] []]
-lexicon "từ" 		= [Cat "tu" 		"PREP" [] []]
-lexicon "dưới"	 	= [Cat "duoi"   	"PREP" [] []]
-lexicon "với" 		= [Cat "voi" 		"PREP" [] []]
-lexicon "bên" 		= [Cat "ben" 		"PREP" [] []]
+lexicon "trong"		= [Cat "trong" 		"PREP" [] []]         -- in
+lexicon "cho"   	= [Cat "cho"   		"PREP" [] []]         -- for
+lexicon "từ" 		= [Cat "tu" 		"PREP" [] []]         -- from
+lexicon "dưới"	 	= [Cat "duoi"   	"PREP" [] []]         -- under
+lexicon "với" 		= [Cat "voi" 		"PREP" [] []]         -- with
+lexicon "bên" 		= [Cat "ben" 		"PREP" [] []]         -- with
 
-lexicon "lên" 		= [Cat "len" 		"AUG" [] []]
-lexicon "ra" 		= [Cat "ra" 		"AUG" [] []]
-lexicon "thấy" 		= [Cat "thay" 		"AUG" [] []]
-lexicon "được" 		= [Cat "duoc" 		"AUG" [] []]
-lexicon "vừa" 		= [Cat "vua" 		"AUG" [] []]
-lexicon "xuống"		= [Cat "xuong" 		"AUG" [] []]
-lexicon "vào" 		= [Cat "vao" 		"AUG" [] []]
+lexicon "lên" 		= [Cat "len" 		"AUG" [] []]         -- up
+lexicon "ra" 		= [Cat "ra" 		"AUG" [] []]         -- out
+lexicon "thấy" 		= [Cat "thay" 		"AUG" [] []]         -- found
+lexicon "được" 		= [Cat "duoc" 		"AUG" [] []]         -- beneficial
+lexicon "vừa" 		= [Cat "vua" 		"AUG" [] []]         -- fit
+lexicon "xuống"		= [Cat "xuong" 		"AUG" [] []]         -- down
+lexicon "vào" 		= [Cat "vao" 		"AUG" [] []]         -- in
 
 -- INTRANS (8)
-lexicon "ăn"		= [Cat "an"			"VP" [] []]
-lexicon "chết"		= [Cat "chet"		"VP" [] []]
-lexicon "chết-đuối"	= [Cat "chet-duoi"	"VP" [] []]
-lexicon "cô-đơn"	= [Cat "co-don"		"VP" [] []]
-lexicon "đi"		= [Cat "di"			"VP" [] []]
-lexicon "ghen-tức"	= [Cat "ghen-tuc"	"VP" [] []]
-lexicon "khóc"		= [Cat "khoc"		"VP" [] []]
-lexicon "mất"		= [Cat "mat"		"VP" [] []]
-lexicon "tức-giận"	= [Cat "tuc-gian"	"VP" [] []]
+lexicon "ăn"		= [Cat "an"			"VP" [] []]         -- eat
+lexicon "chết"		= [Cat "chet"		"VP" [] []]         -- die
+lexicon "chết-đuối"	= [Cat "chet-duoi"	"VP" [] []]         -- drown
+lexicon "cô-đơn"	= [Cat "co-don"		"VP" [] []]         -- alone
+lexicon "đi"		= [Cat "di"			"VP" [] []]         -- go
+lexicon "ghen-tức"	= [Cat "ghen-tuc"	"VP" [] []]         -- envy
+lexicon "khóc"		= [Cat "khoc"		"VP" [] []]         -- cry
+lexicon "mất"		= [Cat "mat"		"VP" [] []]         -- lose
+lexicon "tức-giận"	= [Cat "tuc-gian"	"VP" [] []]         -- angry
 
 -- NP (16)
-lexicon "là"		= [Cat "la"			"VP" [] [Cat "_" "NP" [] []]]
-lexicon "hành-hạ"	= [Cat "hanh-ha"	"VP" [] [Cat "_" "NP" [] []]]
-lexicon "làm-thịt"	= [Cat "lam-thit"	"VP" [] [Cat "_" "NP" [] []]]
-lexicon "mở"		= [Cat "mo"			"VP" [] [Cat "_" "NP" [] []]]
-lexicon "dự"		= [Cat "du"			"VP" [] [Cat "_" "NP" [] []]]
-lexicon "trở-thành"	= [Cat "tro-thanh"	"VP" [] [Cat "_" "NP" [] []]]
-lexicon "về"		= [Cat "ve"			"VP" [] [Cat "_" "NP" [] []]]
-lexicon "nghe"		= [Cat "nghe"		"VP" [] [Cat "_" "NP" [] []]]
-lexicon "hái"		= [Cat "hai"		"VP" [] [Cat "_" "NP" [] []]]
-lexicon "chặt"		= [Cat "chat"		"VP" [] [Cat "_" "NP" [] []]]
-lexicon "giết"		= [Cat "giet"		"VP" [] [Cat "_" "NP" [] []]]
-lexicon "thăm"		= [Cat "tham"		"VP" [] [Cat "_" "NP" [] []]]
-lexicon "gặp"		= [Cat "gap"		"VP" [] [Cat "_" "NP" [] []]]
-lexicon "trở-về"	= [Cat "tro-ve"		"VP" [] [Cat "_" "NP" [] []]]
-lexicon "dội"		= [Cat "doi"		"VP" [] [Cat "_" "NP" [] []]]
-lexicon "mặc"		= [Cat "mac"		"VP" [] [Cat "_" "NP" [] []]]
+lexicon "là"		= [Cat "la"			"VP" [] [Cat "_" "NP" [] []]]         -- is
+lexicon "hành-hạ"	= [Cat "hanh-ha"	"VP" [] [Cat "_" "NP" [] []]]         -- torture
+lexicon "làm-thịt"	= [Cat "lam-thit"	"VP" [] [Cat "_" "NP" [] []]]         -- butcher
+lexicon "mở"		= [Cat "mo"			"VP" [] [Cat "_" "NP" [] []]]         -- open
+lexicon "dự"		= [Cat "du"			"VP" [] [Cat "_" "NP" [] []]]         -- attend
+lexicon "trở-thành"	= [Cat "tro-thanh"	"VP" [] [Cat "_" "NP" [] []]]         -- become
+lexicon "về"		= [Cat "ve"			"VP" [] [Cat "_" "NP" [] []]]         -- back
+lexicon "nghe"		= [Cat "nghe"		"VP" [] [Cat "_" "NP" [] []]]         -- listen
+lexicon "hái"		= [Cat "hai"		"VP" [] [Cat "_" "NP" [] []]]         -- gather
+lexicon "chặt"		= [Cat "chat"		"VP" [] [Cat "_" "NP" [] []]]         -- cut
+lexicon "giết"		= [Cat "giet"		"VP" [] [Cat "_" "NP" [] []]]         -- kill
+lexicon "thăm"		= [Cat "tham"		"VP" [] [Cat "_" "NP" [] []]]         -- visit
+lexicon "gặp"		= [Cat "gap"		"VP" [] [Cat "_" "NP" [] []]]         -- meet
+lexicon "trở-về"	= [Cat "tro-ve"		"VP" [] [Cat "_" "NP" [] []]]         -- back
+lexicon "dội"		= [Cat "doi"		"VP" [] [Cat "_" "NP" [] []]]         -- pour
+lexicon "mặc"		= [Cat "mac"		"VP" [] [Cat "_" "NP" [] []]]         -- wear
 
 -- AP (1)
-lexicon "khen"		= [Cat "khen"		"VP" [] [Cat "_" "ADJ" [] []]]
+lexicon "khen"		= [Cat "khen"		"VP" [] [Cat "_" "ADJ" [] []]]         -- praise
 
 -- PP (1)
-lexicon "ở"			= [Cat "o"			"VP" [] [Cat "_" "PP" [] []]]
-lexicon "quấn-quýt"	= [Cat "quan-quyt"	"VP" [] [Cat "_" "PP" [] []]]
+lexicon "ở"			= [Cat "o"			"VP" [] [Cat "_" "PP" [] []]]         -- stay
+lexicon "quấn-quýt"	= [Cat "quan-quyt"	"VP" [] [Cat "_" "PP" [] []]]         -- become close (with)
 
 -- INTRANS / PP (1)
 lexicon "xem"		= [Cat "xem"		"VP" [] [],
-					   Cat "xem"		"VP" [] [Cat "_" "PP" [] []]]
+					   Cat "xem"		"VP" [] [Cat "_" "PP" [] []]]         -- watch
 
 -- AUG (2)
-lexicon "hiện"		= [Cat "hien"		"VP" [] [Cat "_" "AUG" [] []]]
-lexicon "trèo"		= [Cat "treo"		"VP" [] [Cat "_" "AUG" [] []]]
+lexicon "hiện"		= [Cat "hien"		"VP" [] [Cat "_" "AUG" [] []]]         -- appear
+lexicon "trèo"		= [Cat "treo"		"VP" [] [Cat "_" "AUG" [] []]]         -- climb
 
 -- NP VP (6)
-lexicon "muốn"		= [Cat "muon"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]
-lexicon "sai"		= [Cat "sai"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]
-lexicon "nhờ"		= [Cat "nho"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]
-lexicon "bảo"		= [Cat "bao"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]
-lexicon "linh-cảm"	= [Cat "linh-cam"	"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]
-lexicon "phong"		= [Cat "phong"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]
+lexicon "muốn"		= [Cat "muon"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]         -- want
+lexicon "sai"		= [Cat "sai"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]         -- order
+lexicon "nhờ"		= [Cat "nho"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]         -- ask
+lexicon "bảo"		= [Cat "bao"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]         -- tell
+lexicon "linh-cảm"	= [Cat "linh-cam"	"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]         -- presage
+lexicon "phong"		= [Cat "phong"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]         -- bestow
 
 -- NP / (NP VP) (4)
 lexicon "có"		= [Cat "co"			"VP" [] [Cat "_" "NP" [] []],
-					   Cat "co"			"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]
+					   Cat "co"			"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]         -- have
 lexicon "hóa-thành"	= [Cat "hoa-thanh"	"VP" [] [Cat "_" "NP" [] []],
-					   Cat "hoa-thanh"	"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]
+					   Cat "hoa-thanh"	"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]         -- transform
 lexicon "làm"		= [Cat "lam"		"VP" [] [Cat "_" "NP" [] []],
-					   Cat "lam"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]
+					   Cat "lam"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]         -- work/do
 lexicon "giúp"		= [Cat "giup"		"VP" [] [Cat "_" "NP" [] []],
-					   Cat "giup"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]
+					   Cat "giup"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "VP" [] []]]         -- help
 
 -- NP PP (4)
-lexicon "chôn"		= [Cat "chon"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "PP" [] []]]
-lexicon "trộn"		= [Cat "tron"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "PP" [] []]]
-lexicon "làm-rơi"	= [Cat "lam-roi"	"VP" [] [Cat "_" "NP" [] [], Cat "_" "PP" [] []]]
-lexicon "tặng"		= [Cat "tang"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "PP" [] []]]
+lexicon "chôn"		= [Cat "chon"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "PP" [] []]]         -- burry
+lexicon "trộn"		= [Cat "tron"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "PP" [] []]]         -- mix
+lexicon "làm-rơi"	= [Cat "lam-roi"	"VP" [] [Cat "_" "NP" [] [], Cat "_" "PP" [] []]]         -- drop
+lexicon "tặng"		= [Cat "tang"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "PP" [] []]]         -- give as a gift
 
 -- NP AUG (2)
-lexicon "tách-riêng"= [Cat "tach-rieng"	"VP" [] [Cat "_" "NP" [] [], Cat "_" "AUG" [] []]]
-lexicon "đào"		= [Cat "dao"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "AUG" [] []]]
+lexicon "tách-riêng"= [Cat "tach-rieng"	"VP" [] [Cat "_" "NP" [] [], Cat "_" "AUG" [] []]]         -- split
+lexicon "đào"		= [Cat "dao"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "AUG" [] []]]         -- dig
 
 -- AUG NP (5)
-lexicon "phát-hiện"	= [Cat "phat-hien"	"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]
-lexicon "nhặt"		= [Cat "nhat"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]
-lexicon "ướm"		= [Cat "uom"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]
-lexicon "rơi"		= [Cat "roi"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]
-lexicon "bay"		= [Cat "bay"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]
+lexicon "phát-hiện"	= [Cat "phat-hien"	"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]         -- discover
+lexicon "nhặt"		= [Cat "nhat"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]         -- pick up
+lexicon "ướm"		= [Cat "uom"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]         -- try (shoes)
+lexicon "rơi"		= [Cat "roi"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]         -- drop
+lexicon "bay"		= [Cat "bay"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]         -- fly
 
 -- NP / (AUG NP) (2)
 lexicon "đem"		= [Cat "dem"		"VP" [] [Cat "_" "NP" [] []],
-					   Cat "dem"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]
+					   Cat "dem"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]         -- bring
 lexicon "tìm"		= [Cat "tim"		"VP" [] [Cat "_" "NP" [] []],
-					   Cat "tim"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]
+					   Cat "tim"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]         -- search for
 -- AUG PP (1)
-lexicon "chui"		= [Cat "chui"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "PP" [] []]]
+lexicon "chui"		= [Cat "chui"		"VP" [] [Cat "_" "AUG" [] [], Cat "_" "PP" [] []]]         -- crouch
 
 -- NP AUG NP (1)
-lexicon "thế"		= [Cat "the"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]
+lexicon "thế"		= [Cat "the"		"VP" [] [Cat "_" "NP" [] [], Cat "_" "AUG" [] [], Cat "_" "NP" [] []]]          -- replace
 
 lexicon _ = []
 
